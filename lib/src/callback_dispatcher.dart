6dee936c1a52a71c0b4832972022e9fedee41140
Akos Putz <akos@secoli.tech>
2019-07-21 22:39:56 -0400

    Declaring callbackDispatcher as vm entry point

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -10,16 +10,21 @@ import 'package:flutter/services.dart';
 import 'package:geofencing/src/geofencing.dart';
 import 'package:geofencing/src/location.dart';
 
+@pragma('vm:entry-point')
 void callbackDispatcher() {
   const MethodChannel _backgroundChannel =
       MethodChannel('plugins.flutter.io/geofencing_plugin_background');
   WidgetsFlutterBinding.ensureInitialized();
 
   _backgroundChannel.setMethodCallHandler((MethodCall call) async {
-    final List<dynamic> args = call.arguments;
+    final dynamic args = call.arguments;
+
+    // PluginUtilities.getCallbackFromHandle performs a lookup based on the
+    // callback handle and returns a tear-off of the original callback.
     final Function callback = PluginUtilities.getCallbackFromHandle(
         CallbackHandle.fromRawHandle(args[0]));
     assert(callback != null);
+
     final List<String> triggeringGeofences = args[1].cast<String>();
     final List<double> locationList = <double>[];
     // 0.0 becomes 0 somewhere during the method call, resulting in wrong
